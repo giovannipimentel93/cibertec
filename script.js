c8b070a4ca8be531a7f1230cf72b99bc2a3c7afd
giovannipimentel93 <103405163+giovannipimentel93@users.noreply.github.com>
2023-10-22 19:12:29 -0500

    correciones

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 var popup = document.getElementById("popup");
 var loginButton = document.getElementById("loginButton");
 var myAccountLink = document.getElementById("myAccount");
+var errorMessage = document.getElementById("error-message");
 
 function togglePopup() {
     popup.style.display = popup.style.display === "none" || popup.style.display === "" ? "flex" : "none";
+    errorMessage.textContent = ""; // Limpiar el mensaje de error al mostrar el popup
 }
 
-loginButton.addEventListener("click", togglePopup);
-myAccountLink.addEventListener("click", togglePopup);
+loginButton.addEventListener("click", function() {
+    validateLogin();
+});
+
+myAccountLink.addEventListener("click", function(event) {
+    event.preventDefault(); // Evitar que el enlace "Mi cuenta" redireccione
+    togglePopup();
+});
 
 function validateLogin() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    var errorMessage = document.getElementById("error-message");
 
     if (username === "cibertec" && password === "cibertec") {
-        window.location.href = "https://www.google.com";
+        errorMessage.textContent = "Usuario correcto";
+        setTimeout(function() {
+            window.location.href = "https://www.google.com";
+        }, 1500); // Redireccionar después de 1.5 segundos (1500 milisegundos)
     } else {
         errorMessage.textContent = "Error: Usuario o contraseña incorrectos.";
     }
